7817476424c53333ee30e67572e2ccb6ab30a561
Vince <202410692@gordoncollege.edu.ph>
2026-08-04 12:56:06 +0800

    Add Express server for Render

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Bot is running!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
+
 require("dotenv").config();
 const {
   Client,
